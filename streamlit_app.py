14b29c425fd92f9c9d2caa094699932befb475dd
amrev15 <47102305+amrev15@users.noreply.github.com>
2023-08-30 14:17:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,41 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+import openai_secret_manager
+import openai
+from pdfminer.high_level import extract_text
 
-def generate_response(uploaded_file, openai_api_key, query_text):
-    # Load document if file is uploaded
-    if uploaded_file is not None:
-        documents = [uploaded_file.read().decode()]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.create_documents(documents)
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
-        # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-        return qa.run(query_text)
+with st.sidebar:
+    secrets = openai_secret_manager.get_secret("openai")
+    openai_api_key = secrets["api_key"]
+    "View the source code"
+    "!Open in GitHub Codespaces"
 
+st.title("📝 File Q&A with OpenAI")
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md", "pdf"))
+question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
 
-# Page title
-st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+if uploaded_file and question and not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
 
-# File upload
-uploaded_file = st.file_uploader('Upload an article', type='txt')
-# Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
+if uploaded_file and question and openai_api_key:
+    if uploaded_file.type == "application/pdf":
+        article = extract_text(uploaded_file)
+    else:
+        article = uploaded_file.read().decode()
+    prompt = f"Here's an article:\n\n<article>\n{article}\n\n</article>\n\n{question}\n"
 
-# Form input and query
-result = []
-with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
-            result.append(response)
-            del openai_api_key
-
-if len(result):
-    st.info(response)
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    answer = response["choices"][0]["text"]
+    
+    st.write("### Answer")
+    st.write(answer)
