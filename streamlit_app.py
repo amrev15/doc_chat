9d9878c4c928d56f595077ddbf015807b58a4ddb
amrev15 <47102305+amrev15@users.noreply.github.com>
2023-08-30 15:03:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if uploaded_file and question and openai_api_key:
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
-        max_tokens=150,
+        max_tokens=100,
         n=1,
         stop=None,
         temperature=0.5,
